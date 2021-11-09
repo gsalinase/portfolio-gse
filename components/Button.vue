d17f0b50cd505c7bf8af89803be0d28de1cef508
Gonzalo Salinas <gsalinase91@gmail.com>
2021-11-08 21:32:39 -0300

    Added Component

---
+++ components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <a class="button button_primary" :href="link">
+    <slot />
+  </a>
+</template>
+
+<script>
+  export default {
+    props: {
+      link: {
+        type: String,
+        default: '#'
+      },
+      classes: {
+        type: String,
+        default: 'button button_primary'
+      },
+    },
+  }
+</script>
+
+<style lang="postcss" scoped>
+.button {
+  padding: 1rem 1.5rem;
+}
+
+.button_primary {
+  text-decoration: none;
+  font-family: var(--montserrat);
+  background-color: var(--sunset-orange);
+  color: var(--white); 
+}
+
+.button_primary:hover {
+  background-color: #b14040;
+}
+</style>
